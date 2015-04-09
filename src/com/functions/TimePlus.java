da4902a22917cd9099adc92d56a478d1c7686bbb
billy119 <billy7x17@gmail.com>
2015-04-09 22:35:55 +0800

    时间计算类

---
+++ src/com/functions/TimePlus.java
@@ -0,0 +1,14 @@
+package com.functions;
+
+public class TimePlus
+{
+	public String plus(String time1 , String time2)
+	{
+		return null;
+	}
+
+	public String minus(String time1 , String time2)
+	{
+		return null;
+	}
+}
