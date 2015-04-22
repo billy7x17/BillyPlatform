5381b7c0416d5d5f25590b18d234efe3a59b6381
billy119 <billy7x17@gmail.com>
2015-04-22 23:03:33 +0800

    新增飞机航班的相关json

--- src/com/test/test.js
+++ src/com/test/test.js
@@ -1,4 +1,4 @@
-var json = {
+var trainJson = {
 	"code" : 305000,
 	"text" : "亲，已帮您找到列车信息",
 	"list" : [ {
@@ -17,5 +17,86 @@ var json = {
 		"endtime" : "15:28",
 		"icon" : "http://unidust.cn/images/api-train.jpg",
 		"detailurl" : "http://touch.qunar.com/h5/train/"
+	} ]
+};
+
+var flightJson = {
+	"code" : 306000,
+	"text" : "亲，已帮您找到航班信息",
+	"list" : [ {
+		"flight" : "MU3792 东方航空 共享",
+		"starttime" : "20:10",
+		"endtime" : "23:00",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "HU7292 海南航空",
+		"starttime" : "20:35",
+		"endtime" : "23:25",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "CA1816 中国国航",
+		"starttime" : "20:15",
+		"endtime" : "23:10",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "ZH1816 深圳航空 共享",
+		"starttime" : "20:15",
+		"endtime" : "23:10",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "ZH1834 深圳航空 共享",
+		"starttime" : "15:30",
+		"endtime" : "18:25",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "ZH1810 深圳航空 共享",
+		"starttime" : "12:45",
+		"endtime" : "15:40",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "CA1834 中国国航",
+		"starttime" : "15:30",
+		"endtime" : "18:25",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "CA1810 中国国航",
+		"starttime" : "12:45",
+		"endtime" : "15:40",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "HU7192 海南航空",
+		"starttime" : "13:05",
+		"endtime" : "15:55",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "CZ378 南方航空",
+		"starttime" : "12:05",
+		"endtime" : "14:45",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "CA1802 中国国航",
+		"starttime" : "07:50",
+		"endtime" : "10:40",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "CA1872 中国国航",
+		"starttime" : "17:55",
+		"endtime" : "20:40",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "CZ5071 南方航空 共享",
+		"starttime" : "21:30",
+		"endtime" : "00:20 +1天",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "ZH1872 深圳航空 共享",
+		"starttime" : "17:55",
+		"endtime" : "20:40",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
+	}, {
+		"flight" : "CZ5085 南方航空 共享",
+		"starttime" : "07:00",
+		"endtime" : "09:35",
+		"icon" : "http://unidust.cn/images/api-flight.jpg"
 	} ]
 };
